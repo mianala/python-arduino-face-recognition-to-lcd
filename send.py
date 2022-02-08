7b57a83279468fc41e99a99e554aded0fcb5d383
Loharano <mianalaloharano@gmail.com>
2022-02-08 16:08:53 +0300

    working after 3s of initialization

--- send.py
+++ send.py
@@ -2,11 +2,19 @@ from serial import Serial
 import time
 
 # windows
+# connecting to the arduino
 display = Serial(port='COM3', baudrate=9600, timeout=.1)
 
 print(display.name)
-display.write(b'Hello')
+time.sleep(4)
 
+display.write(b'Hello')
+time.sleep(4)
+
+display.write(b'Mianala')
+time.sleep(4)
+
+display.write(b'Mandeha')
 # while True:
 #     print(display.readline())
 #     display.write(b'Hello')
